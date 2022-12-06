0eba0399dc72d0afcf146befaa761aa6aea1999f
Arun <arun.motoori@gmail.com>
2022-12-06 14:35:09 +0530

    Updated Comment in Search.java file.

--- src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
+++ src/test/java/com/tutorialsninja/qa/testcases/SearchTest.java
@@ -10,6 +10,8 @@ import com.tutorialsninja.qa.base.Base;
 import com.tutorialsninja.qa.pages.HomePage;
 import com.tutorialsninja.qa.pages.SearchPage;
 
+//Updated Comment
+
 public class SearchTest extends Base {
 	
 	SearchPage searchPage;
